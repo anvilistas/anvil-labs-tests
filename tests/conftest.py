eb9f2bf5c830450a753eb4e9895a2089fc5a17fb
Owen Campbell <owen.campbell@empiria.co.uk>
2022-05-12 16:56:49 +0100

    Fix disconnect

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,15 @@ APP_URL = "ws://app:3030/_/uplink"
 @pytest.fixture
 def client_uplink():
     anvil.server.connect("client-uplink-key", url=APP_URL, quiet=True)
+    yield
+    anvil.server.disconnect()
 
 
 @pytest.fixture
 def uplink():
     anvil.server.connect("uplink-key", url=APP_URL, quiet=True)
+    yield
+    anvil.server.disconnect()
 
 
 @pytest.fixture
@@ -20,5 +24,3 @@ def clean_db(uplink):
     tables = ("current", "events", "projections", "sequences")
     for table in tables:
         getattr(app_tables, table).delete_all_rows()
-    yield
-    anvil.server.disconnect()
