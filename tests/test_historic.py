ea9922701d8db99c2e4c18b7fbe63c160fd405e0
Owen Campbell <owen.campbell@empiria.co.uk>
2022-05-12 12:59:04 +0100

    use db for dummy test

--- tests/test_historic.py
+++ tests/test_historic.py
@@ -1,2 +1,7 @@
-def test_dummy(clean_db):
-    assert False
+from anvil.tables import app_tables
+
+
+def test_dummy(clean_db, uplink):
+    app_tables.events.add_row(event_id=42, event_type="hello")
+    rows = app_tables.events.search()
+    assert len(rows) == 1
